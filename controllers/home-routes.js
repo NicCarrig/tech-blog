7ad92bc039969634c708135e7c2a04ba6bd0be14
unknown <clemcarrig90@hotmail.com>
2022-07-24 03:49:35 -0700

    post editing working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -116,7 +116,7 @@ router.get('/dashboard', withAuth, (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain:true }));
-      res.render('dashboard', {loggedIn: true});
+      res.render('dashboard', {posts, loggedIn: true});
     })
     .catch( err => {
       console.log(err);
@@ -125,6 +125,45 @@ router.get('/dashboard', withAuth, (req, res) => {
 })
 
 
+router.get('/dashboard/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'post_body',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 
 
